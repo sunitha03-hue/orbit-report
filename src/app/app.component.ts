c09de4335128832ba43f42bc22e8a3626024739d
sunitha <sunitha>
2020-08-07 19:33:10 -0500

    Started step5

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,6 +12,22 @@ export class AppComponent {
   title = 'orbit-report';
   sourceList: Satellite[];
   constructor() {
+   this.sourceList = [];
+   let satellitesUrl = 'https://handlers.education.launchcode.org/static/satellites.json';
+
+   window.fetch(satellitesUrl).then(function(response) {
+      response.json().then(function(data) {
+
+         let fetchedSatellites = data.satellites;
+         // TODO: loop over satellites
+         // TODO: create a Satellite object using new Satellite(fetchedSatellites[i].name, fetchedSatellites[i].type, fetchedSatellites[i].launchDate, fetchedSatellites[i].orbitType, fetchedSatellites[i].operational);
+         // TODO: add the new Satellite object to sourceList using: this.sourceList.push(satellite);
+
+      }.bind(this));
+   }.bind(this));
+
+}
+  constructor() {
     this.sourceList = [
        new Satellite("SiriusXM", "Communication", "2009-03-21", "LOW", true),
        new Satellite("Cat Scanner", "Imaging", "2012-01-05", "LOW", true),
