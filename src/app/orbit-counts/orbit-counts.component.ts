d18d4e62a17527b14feac186619135cdb7cf81e7
sunitha <sunitha>
2020-08-09 18:52:56 -0500

    Completed Assignment 6

--- src/app/orbit-counts/orbit-counts.component.ts
+++ src/app/orbit-counts/orbit-counts.component.ts
@@ -1,12 +1,12 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Input } from '@angular/core';
+import { Satellite } from '../satellite'
 @Component({
   selector: 'app-orbit-counts',
   templateUrl: './orbit-counts.component.html',
   styleUrls: ['./orbit-counts.component.css']
 })
 export class OrbitCountsComponent implements OnInit {
-
+  @Input() satellites: Satellite[];
   constructor() { }
 
   ngOnInit() {
